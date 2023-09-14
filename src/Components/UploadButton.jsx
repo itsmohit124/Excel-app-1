423ce6ca2aed62e9ebd4769541d4f0afd2cd28ff
itsmohit124 <itsmohit124@gmail.com>
2023-09-15 01:55:51 +0530

    second update

--- src/Components/UploadButton.jsx
+++ src/Components/UploadButton.jsx
@@ -107,6 +107,12 @@
 
 // export default UploadButton;
 
+
+
+
+// For both drag drop and click api calls
+
+
 import React, { useState } from 'react';
 import { Button, Box, LinearProgress, Snackbar, Typography } from '@mui/material';
 
@@ -150,5 +156,90 @@ function UploadButton({ onUpload }) {
     reader.readAsBinaryString(file);
   };
 
-  // ... rest of your component
+  const handleClosePopup = () => {
+    setSuccessPopup(false);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const files = e.dataTransfer.files;
+    if (files.length > 0) {
+      handleUpload(files[0]);
+    }
+  };
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      handleUpload(file);
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <h1>Upload a file</h1>
+        <div className="upload-container">
+          <div
+            className="border-container"
+            style={{
+              padding: '100px',
+              backgroundColor: '#f1f1f1',
+              border: '2px dashed rgba(198, 198, 198, 0.65)',
+              borderRadius: '10px',
+              textAlign: 'center',
+              cursor: 'pointer',
+            }}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          >
+            <Typography variant="h6" gutterBottom>
+              Drag and drop files here
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+              or
+            </Typography>
+            <label htmlFor="file-upload" style={{ cursor: 'pointer', color: 'blue' }}>
+              <Button
+                variant="contained"
+                component="span"
+                style={{ borderRadius: '6px', backgroundColor: '#1976d2', color: 'white', padding: '10px 20px' }}
+              >
+                Browse
+              </Button>
+            </label>
+            <input
+              type="file"
+              accept=".xlsx, .xls"
+              onChange={handleFileInputChange}
+              style={{ display: 'none' }}
+              id="file-upload"
+            />
+          </div>
+        </div>
+      </div>
+
+      {uploading && (
+        <div style={{ textAlign: 'center', marginTop: '10px' }}>
+          <LinearProgress />
+        </div>
+      )}
+
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        open={successPopup}
+        autoHideDuration={3000}
+        onClose={handleClosePopup}
+        message="File has been uploaded successfully"
+      />
+    </div>
+  );
 }
+
+export default UploadButton;
